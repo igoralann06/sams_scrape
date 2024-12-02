620cbad8f173bbc8c0499c74d6fc4f168b2805bc
Igor <igoralann113@gmail.com>
2024-12-03 00:31:11 +0900

    updated

--- main.py
+++ main.py
@@ -100,7 +100,6 @@ if __name__ == '__main__':
     departments = get_departments(cookies)
     secondaries = get_secondaries(cookies, departments)
     other_secondaries = get_categories(cookies, secondaries)
-    print(len(other_secondaries))
     secondaries1 = get_categories(cookies, other_secondaries)
     
     
